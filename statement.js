eeeb141bb54728a3861b42822a30c8b49feac3f3
aginga <brianaginga35@gmail.com>
2023-02-11 10:24:27 -0800

    new lines of Code

---
+++ statement.js
@@ -0,0 +1,17 @@
+
+if (false || "" || NaN || 0 || undefined) {
+    console.log('is the same');
+}
+else{
+    console.log('is not the same all are false');
+}
+
+
+if (true && "false" && 1 && -1 ) {
+    console.log('is true');
+}
+else{
+    console.log('is not true');
+}
+
+
