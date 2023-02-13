e43d9b696a48d261c72371fee83d4df0a6f00472
aginga <brianaginga35@gmail.com>
2023-02-12 16:25:05 -0800

    prototype concept included

--- functioncon.js
+++ functioncon.js
@@ -5,6 +5,14 @@ function Circle (radius ){
     this.radius = radius;
 }
 
+//the use of prototype
+
+Circle.prototype.getArea = function () {
+
+    return Math.PI * Math.pow(this.radius, 2);
+}
+
 var myCircle = new Circle(10);
 
-console.log(myCircle);
+console.log(myCircle.getArea());
+
