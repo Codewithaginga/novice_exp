93a51b517ad508ed6079494666806e6baca74409
aginga <brianaginga35@gmail.com>
2023-02-14 14:19:58 -0800

    practice

--- ar.js
+++ ar.js
@@ -16,3 +16,10 @@ let dream = {
 for ( let ls in dream ) {
     console.log(ls + ":" + dream[ls]);
 }
+
+var counter = 0;
+var myArray = ["Yaakov", 2, {handle: "yaakovchaikin"}];
+for (var i = 0; i < myArray.length; i++) {
+  counter++;
+}
+console.log(counter);
