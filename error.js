d549691d5ad54eadb53a7e3219f845d29c5013f1
aginga <brianaginga35@gmail.com>
2023-02-22 16:46:38 -0800

    added lines of code

---
+++ error.js
@@ -0,0 +1,14 @@
+try {
+
+    console.log(a+b);
+}
+
+catch(err){
+    console.log(err);
+
+    console.log('An error was identify');
+    console.log('The server is working on it');
+}
+
+console.log('But this doesn not stop our code ');
+
